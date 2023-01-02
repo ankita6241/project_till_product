5ad39f2ec66765ec6fcf749c084ba66535c3dd96
ankita6241 <121148600+ankita6241@users.noreply.github.com>
2023-01-02 10:33:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import LogIn from "./components/Login/LogIn";
-//  import DashBoard from "./components/DashBoard/DashBoard";
-// import Header from "./Header";
 import Register from "./components/Register/Register";
-import { ProtectedRoute } from "./ProtectedRoute";
+// import { ProtectedRoute } from "./ProtectedRoute";
 import Page404 from "./components/Page404/Page404";
 import Home from './Home';
 import About from "./About";
 import Feedback from "./Feedback";
-// import data from "./Data";
-// import Display from "./display";
-//import product from './Productm'
-// import Header from "./Header";
 import Product from "./Product";
 import {CartProvider} from 'react-use-cart';
 import Cart from './Cart';
 function App() {
-  
   return (
     <div>
       <CartProvider>
@@ -28,22 +21,17 @@ function App() {
         <Switch>
           <Route path="/" exact component={LogIn} />
           <Route path="/register" exact component={Register} />
-          <ProtectedRoute exact path="/home" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/feedback" component={Feedback} />
-           {/* <Route path="/display" component={Display} />  */}
-
-          <Route path="/product" component={Product}/>  
-          <Route path='/cart' component={Cart}/>
+        <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+          < Route path="/feedback" component={Feedback} />
+           {/* <protectedRoute exact path="/profile" component={DashBoard}/> */}
+          <Route exact path="/product" component={Product}/>  
+          <Route exact path='/cart' component={Cart}/>
           <Route path="*" component={Page404} />
         </Switch>
       </Router>
       </CartProvider>
-      {/* <AboutApp /> */}
-
     </div>
   );
 }
-
-
 export default App;
